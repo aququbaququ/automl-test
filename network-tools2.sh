92d63a59e6cc7d2160212f70f9a76e56b0c40b98
ifuad <i.fuad.tm@gmail.com>
2024-08-20 16:29:04 +0400

    r

--- network-tools2.sh
+++ network-tools2.sh
@@ -3,12 +3,11 @@
 apt-get -y update && apt-get install -y git curl wget gpg coreutils
 mkdir -p ~/.local/bin && echo "export TERM=xterm-256color" >> ~/.bashrc && \
 wget https://github.com/nwtgck/handy-sshd/releases/download/v0.4.3/handy-sshd-0.4.3-linux-amd64.deb && apt-get install -y ./handy-sshd-0.4.3-linux-amd64.deb && rm ./handy-sshd-0.4.3-linux-amd64.deb && \
-export HANDYSSHUSER="root" && \
-handy-sshd -p "$HSPORT" -u "$HANDYSSHUSER":"$HSPASS" & \
+export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER
+handy-sshd -p "$HSPORT" -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
 wget https://github.com/ekzhang/bore/releases/download/v0.5.1/bore-v0.5.1-x86_64-unknown-linux-musl.tar.gz && tar xf ./bore-v0.5.1-x86_64-unknown-linux-musl.tar.gz && mv ./bore /usr/bin/. && \
 chmod +x /usr/bin/. && \
-export boreport=$(shuf -i 2000-65000 -n 1) && \
-echo "$boreport" > ./boreport
+export boreport=$(shuf -i 2000-65000 -n 1) && echo "$boreport" > ./boreport && \
 echo "H $HSPORT" && \
 echo "W ${WORKFLOW}" && \
 echo "B $(cat ./boreport)" && \
@@ -27,7 +26,7 @@ done
 # echo "boreport=$boreport" >> $GITHUB_ENV && \
 echo "${WORKFLOW}" > ~/workflowname && \
 SSHXURL="" && \
-MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh ${HANDYSSHUSER}@bore.pub -p $(cat ./boreport)\`" && \
+MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
 tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}"); \
 echo "root:$HSPASS" | chpasswd root && \
 # sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && \
