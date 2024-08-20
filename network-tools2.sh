5b22ca43420079963752932c1f4656537b3866f9
ifuad <i.fuad.tm@gmail.com>
2024-08-20 16:24:55 +0400

    bport ref

--- network-tools2.sh
+++ network-tools2.sh
@@ -8,24 +8,26 @@ handy-sshd -p "$HSPORT" -u "$HANDYSSHUSER":"$HSPASS" & \
 wget https://github.com/ekzhang/bore/releases/download/v0.5.1/bore-v0.5.1-x86_64-unknown-linux-musl.tar.gz && tar xf ./bore-v0.5.1-x86_64-unknown-linux-musl.tar.gz && mv ./bore /usr/bin/. && \
 chmod +x /usr/bin/. && \
 export boreport=$(shuf -i 2000-65000 -n 1) && \
+echo "$boreport" > ./boreport
 echo "H $HSPORT" && \
 echo "W ${WORKFLOW}" && \
-echo "B ${boreport}" && \
-nohup bore local -t bore.pub -p "$boreport" "$HSPORT" & \
+echo "B $(cat ./boreport)" && \
+nohup bore local -t bore.pub -p "$(cat ./boreport)" "$HSPORT" & \
 cmdpid=$! && \
 sleep 3 && \
 # while [ ! ps -p $cmdpid > /dev/null ]; do boreport=$(shuf -i 2000-65000 -n 1); nohup bore local -t bore.pub -p $boreport $HSPORT &; cmdpid=$!; sleep 3; done;
 while ! ps -p $cmdpid > /dev/null
 do
   export boreport=$(shuf -i 2000-65000 -n 1)
-  nohup bore local -t bore.pub -p $boreport $HSPORT &
+  echo "$boreport" > ./boreport
+  nohup bore local -t bore.pub -p "$(cat ./boreport)" $HSPORT &
   cmdpid=$!
   sleep 3
 done
 # echo "boreport=$boreport" >> $GITHUB_ENV && \
 echo "${WORKFLOW}" > ~/workflowname && \
 SSHXURL="" && \
-MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh ${HANDYSSHUSER}@bore.pub -p ${boreport}\`" && \
+MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh ${HANDYSSHUSER}@bore.pub -p $(cat ./boreport)\`" && \
 tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}"); \
 echo "root:$HSPASS" | chpasswd root && \
 # sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config && \
