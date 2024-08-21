20198e70db0779e1cb7bb480f3d2634c4b1ab73c
ifuad <i.fuad.tm@gmail.com>
2024-08-21 10:51:41 +0400

    r

--- run-app.sh
+++ run-app.sh
@@ -10,19 +10,20 @@ pm2 start ${APP}  && \
 # echo test | tee -a ./data/test-${APP}.md  && \
 export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER  && \
 handy-sshd -p "$HSPORT" -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
-# export boreport=$(shuf -i 2000-65000 -n 1) && echo "$boreport" > ./boreport && \
-#nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 >bore.log 2>&1 & \
-# cmdpid=$! && \
-# sleep 3 && \
-#while ! ps -p $cmdpid > /dev/null
-#do
-#    export boreport=$(shuf -i 2000-65000 -n 1)
-#    echo "$boreport" > ./boreport
-#    nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
-#    cmdpid=$!
-#    sleep 3
-#done
-# MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
-# tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}");
-#sleep 1h
+export boreport=$(shuf -i 2000-65000 -n 1) && echo "$boreport" > ./boreport && \
+# nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 >bore.log 2>&1 & \
+nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 & \
+cmdpid=$! && \
+sleep 3 && \
+while ! ps -p $cmdpid > /dev/null
+do
+   export boreport=$(shuf -i 2000-65000 -n 1)
+   echo "$boreport" > ./boreport
+   nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
+   cmdpid=$!
+   sleep 3
+done
+MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
+tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}");
+# sleep 1h
 echo "Inside-Docker"
