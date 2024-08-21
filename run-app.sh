9670ae4aab36ada82eaeda75a565688f648eea8c
ifuad <i.fuad.tm@gmail.com>
2024-08-21 09:51:50 +0400

    move nore outside of docker

--- run-app.sh
+++ run-app.sh
@@ -6,8 +6,8 @@ exec bash && \
 cd ./$SGHDIR && git pull && \
 cd ./data && git switch main && git pull --rebase --autostash && cd ..  && \
 pm2 delete all && cd ./data && git reset --hard HEAD && cd ..  && \
-pm2 start ${app}  && \
-# echo test | tee -a ./data/test-${app}.md  && \
+pm2 start ${APP}  && \
+# echo test | tee -a ./data/test-${APP}.md  && \
 export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER  && \
 handy-sshd -p "$HSPORT" -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
 export boreport=$(shuf -i 2000-65000 -n 1) && echo "$boreport" > ./boreport && \
@@ -22,9 +22,7 @@ sleep 3 && \
 #    cmdpid=$!
 #    sleep 3
 #done
-MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
+# MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
 # tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}");
-curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}"
 #sleep 1h
 echo "Inside-Docker"
-nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 & 
