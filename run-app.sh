58524b11ba6a4c9efcde6591b40e390ae522d1ab
ifuad <i.fuad.tm@gmail.com>
2024-08-21 11:48:23 +0400

    r

--- run-app.sh
+++ run-app.sh
@@ -12,7 +12,7 @@ export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER  && \
 handy-sshd -p "$HSPORT" -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
 export boreport=$(shuf -i 2000-65000 -n 1) && echo "$boreport" > ./boreport && \
 # nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 >bore.log 2>&1 & \
-# nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 & \
+nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 & \
 # cmdpid=$! && \
 # sleep 3 && \
 # while ! ps -p $cmdpid > /dev/null
@@ -28,5 +28,5 @@ MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pu
 # sleep 1h
 
 curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}"
-nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
+# nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
 echo "Inside-Docker"
