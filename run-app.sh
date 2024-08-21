9b425f53555ceed3dad39c686000d09c26e738f8
ifuad <i.fuad.tm@gmail.com>
2024-08-21 15:05:45 +0400

    ssh

--- run-app.sh
+++ run-app.sh
@@ -3,7 +3,7 @@
 echo $USER
 # exec bash && \
 echo "root:$HSPASS" | chpasswd root
-# service ssh start
+service ssh start
 cd /$SGHDIR && git pull
 ls ./ | grep ${APP} | echo
 cd ./data && git switch main && git pull --rebase --autostash && cd ..;
@@ -12,7 +12,7 @@ mise x -- pm2 start "${APP}"
 # echo test | tee -a ./data/test-${APP}.md  && \
 export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER
 # handy-sshd -p "$HSPORT" -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
-handy-sshd -p 22 -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
+# handy-sshd -p 22 -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
 export boreport=$(shuf -i 2000-65000 -n 1)
 echo "$boreport" > ./boreport
 # nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 >bore.log 2>&1 & \
