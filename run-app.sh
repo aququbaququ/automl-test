fd715e2fa84e372fb222fc6173de99c20eda717e
ifuad <i.fuad.tm@gmail.com>
2024-08-21 12:49:25 +0400

    all in exec bash

--- run-app.sh
+++ run-app.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-echo "root:$HSPASS" | chpasswd root && \
-service ssh start 
 echo $USER
 exec bash && \
+echo "root:$HSPASS" | chpasswd root && service ssh start && \
 cd ./$SGHDIR && git pull && \
-cd ./data && git switch main && git pull --rebase --autostash && cd ..  && \
+cd ./data && git switch main && git pull --rebase --autostash && cd .. && \
 pm2 delete all && cd ./data && git reset --hard HEAD && cd ..  && \
-pm2 start ${APP}  && \
+pm2 start ${APP} && \
 # echo test | tee -a ./data/test-${APP}.md  && \
-export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER  && \
+export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER && \
 # handy-sshd -p "$HSPORT" -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
 # handy-sshd -p 22 -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
-export boreport=$(shuf -i 2000-65000 -n 1)
-echo "$boreport" > ./boreport
+export boreport=$(shuf -i 2000-65000 -n 1) && \
+echo "$boreport" > ./boreport && \
 # nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 >bore.log 2>&1 & \
+MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
+curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}" && \
 nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
 # cmdpid=$! && \
 # sleep 3 && \
@@ -26,10 +27,11 @@ nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
 #    cmdpid=$!
 #    sleep 3
 # done
-MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`"
+# exec bash && \
+# MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
 # tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}");
 # sleep 1h
 
-curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}"
+# curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}" && \
 # nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
 echo "Inside-Docker"
