d23afba0db803c6eca373aa3a8bde1e9a7020180
i.fuad.tm <i.fuad.tm@gmail.com>
2024-08-21 07:47:49 +0400

    sleep change

--- run-app.sh
+++ run-app.sh
@@ -24,4 +24,4 @@ sleep 3 && \
 #done
 MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`" && \
 tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}");
-sleep 1h
+#sleep 1h
