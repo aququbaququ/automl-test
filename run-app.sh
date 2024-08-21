42228be633051bc74fefe0c38b8b45210c338da8
ifuad <i.fuad.tm@gmail.com>
2024-08-21 14:36:07 +0400

    handy ssh again

--- run-app.sh
+++ run-app.sh
@@ -3,7 +3,7 @@
 echo $USER
 # exec bash && \
 echo "root:$HSPASS" | chpasswd root
-service ssh start
+# service ssh start
 cd /$SGHDIR && git pull
 ls ./ | grep ${APP} | echo
 cd ./data && git switch main && git pull --rebase --autostash && cd ..;
@@ -12,13 +12,12 @@ mise x -- pm2 start "${APP}"
 # echo test | tee -a ./data/test-${APP}.md  && \
 export HANDYSSHUSER="root" && echo "$HANDYSSHUSER" > ./HANDYSSHUSER
 # handy-sshd -p "$HSPORT" -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
-# handy-sshd -p 22 -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
+handy-sshd -p 22 -u "$(cat ./HANDYSSHUSER)":"$HSPASS" & \
 export boreport=$(shuf -i 2000-65000 -n 1)
 echo "$boreport" > ./boreport
-echo "before-bore 2: $HANDYSSHUSER, $(cat ./boreport)"
 # nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 >bore.log 2>&1 & \
 MSG="wf: ${WORKFLOW} - ${APP}; boressh:  \`boldssh $(cat ./HANDYSSHUSER)@bore.pub -p $(cat ./boreport)\`"
-curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}"
+tmpvar=$(curl -sX POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" -d "disable_web_page_preview=True" -d "parse_mode=Markdown" -d "chat_id=${TELEGRAM_CHAT_ID}" -d "text=${MSG}");
 echo "before-bore"
 nohup bore local -t bore.pub -p "$(cat ./boreport)" 22 &
 # cmdpid=$! && \
