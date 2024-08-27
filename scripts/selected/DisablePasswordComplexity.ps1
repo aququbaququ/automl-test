071a4e4dcf5bcbbfd90d07730b16922e731de17e
ifuad <i.fuad.tm@gmail.com>
2024-08-27 10:47:04 +0400

    rd

--- scripts/selected/DisablePasswordComplexity.ps1
+++ scripts/selected/DisablePasswordComplexity.ps1
@@ -1,3 +1,4 @@
+#Requires -Version 3.0
 secedit /export /cfg c:\secpol.cfg
 (gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
 secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
